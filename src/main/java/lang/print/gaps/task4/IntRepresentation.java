36aaa7f00268b056e3e8ecf34e85cc3040a49f3f
edvinasvencevicius <edviz.nzn@gmail.com>
2023-03-31 17:04:44 +0300

    amen

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,20 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+
+        char c1 = 'c';
+        char c2 = 'h';
+        char c3 = 'a';
+        char c4 = 'r';
+
+        int a1 = (int) c1;
+        int a2 = (int) c2;
+        int a3 = (int) c3;
+        int a4 = (int) c4;
+
+        System.out.println(a1);
+        System.out.println(a2);
+        System.out.println(a3);
+        System.out.println(a4);
     }
 }
